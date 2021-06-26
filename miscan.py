7eb939d9260ea8f1a3df02db19d195bdbe8e7d1a
Unknown <wfremout+github@gmail.com>
2021-06-26 23:06:39 +0200

    remove colours in console output

--- miscan.py
+++ miscan.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python
 import argparse
-import os
 import sys
 from bluepy import btle
 
 from devices.base import Device
-
-if os.getenv('C', '1') == '0':
-    ANSI_RED = ''
-    ANSI_GREEN = ''
-    ANSI_YELLOW = ''
-    ANSI_CYAN = ''
-    ANSI_WHITE = ''
-    ANSI_OFF = ''
-else:
-    ANSI_CSI = "\033["
-    ANSI_RED = ANSI_CSI + '31m'
-    ANSI_GREEN = ANSI_CSI + '32m'
-    ANSI_YELLOW = ANSI_CSI + '33m'
-    ANSI_CYAN = ANSI_CSI + '36m'
-    ANSI_WHITE = ANSI_CSI + '37m'
-    ANSI_OFF = ANSI_CSI + '0m'
-
 
 class ScanDelegate(btle.DefaultDelegate):
 
@@ -44,25 +26,25 @@ class ScanDelegate(btle.DefaultDelegate):
         if dev.addr.startswith("a4:c1:38:") or dev.addr.startswith("58:2d:34:"):
             print ('    Device (%s): %s (%s), %d dBm %s' %
                 (status,
-                    ANSI_WHITE + dev.addr + ANSI_OFF,
+                    dev.addr,
                     dev.addrType,
                     dev.rssi,
                     ('' if dev.connectable else '(not connectable)'))
                 )
             for (sdid, desc, val) in dev.getScanData():
                 if sdid in [0x8, 0x9]:
-                    print ('\t' + desc + ': \'' + ANSI_CYAN + val + ANSI_OFF + '\'')               
+                    print ('\t' + desc + ': \'' + val + '\'')               
                 if sdid in [0x16]:
-                    print ('\t' + desc + ': \'' + ANSI_CYAN + val + ANSI_OFF + '\'')
+                    print ('\t' + desc + ': \''  + val + '\'')
                     parsed = Device().parse(dev.addr, val)
                     if 'format' in parsed:
-                        print ('\t    - format: ' + ANSI_YELLOW + parsed['format'] + ANSI_OFF)
+                        print ('\t    - format: ' + parsed['format'])
                     if 'temperature' in parsed:
-                        print ('\t    - temperature: ' + ANSI_YELLOW + str(parsed['temperature']) + ANSI_OFF + ' °C')
+                        print ('\t    - temperature: ' + str(parsed['temperature']) + ' °C')
                     if 'humidity' in parsed:
-                        print ('\t    - humidity: ' + ANSI_YELLOW + str(parsed['humidity']) + ANSI_OFF + ' %RH')
+                        print ('\t    - humidity: ' + str(parsed['humidity']) + ' %RH')
                     if 'battery' in parsed:
-                        print ('\t    - battery: ' + ANSI_YELLOW + str(parsed['battery']) + ANSI_OFF + ' %')
+                        print ('\t    - battery: ' + str(parsed['battery']) + ' %')
             if not dev.scanData:
                 print ('\t(no data)')
             print
